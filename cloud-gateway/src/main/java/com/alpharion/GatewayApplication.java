a71db32709a9b469473827dff98cc07bee52c0ba
XieYT <xieyt@yunheit.com>
2024-04-13 19:20:14 +0800

    SpringCloud Gateway: 环境搭建

---
+++ cloud-gateway/src/main/java/com/alpharion/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.alpharion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关
+ *
+ * @author XieYT
+ * @since 2024/04/13 下午7:02
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}

31f756d23c038e7476a55a7e3c6c38088d2e7e0e
XieYT <xieyt@yunheit.com>
2024-04-20 16:53:40 +0800

    SpringCloud Gateway: 过滤器(内置)

--- cloud-provider-payment/src/main/java/com/alpharion/controller/PayGatewayController.java
+++ cloud-provider-payment/src/main/java/com/alpharion/controller/PayGatewayController.java
@@ -7,11 +7,14 @@ import com.alpharion.result.R;
 import com.alpharion.result.ReturnCODE;
 import com.alpharion.service.PayService;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Enumeration;
 
 /**
  * 支付网关Controller
@@ -40,5 +43,24 @@ public class PayGatewayController {
     @GetMapping(value = "/info", name = "支付网关信息")
     public R<String> info() {
         return R.success("支付网关信息: " + IdUtil.simpleUUID().toUpperCase());
+    }
+
+    @GetMapping(value = "/filter", name = "支付网关过滤")
+    public R<String> getGatewayFilter(HttpServletRequest request) {
+        int space = 20;
+        StringBuilder result = new StringBuilder();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        // 右对齐，左边填充空格
+        String paddedRight = String.format("%-" + space + "s", "---请求头名称---");
+        System.out.println(paddedRight + "---请求头值---");
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(String.format("%-" + space + "s", name) + value);
+            if (name.contains("Miracle") || name.contains("miracle")) {
+                result.append(name).append("=").append(value).append(" ");
+            }
+        }
+        return R.success("GatewayFilter过滤器测试：" + result + " " + IdUtil.simpleUUID().toUpperCase());
     }
 }
